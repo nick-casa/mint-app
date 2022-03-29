ed2f93e42abfcc4ca607b33150e4bb9ae39f6bb1
nick-casa <nc16@princeton.edu>
2022-03-29 10:14:53 -0400

    setup Next.js App

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import WalletConnect from "@walletconnect/web3-provider";
+import Web3Modal from "web3modal";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export const providerOptions = {
+ walletlink: {
+   package: CoinbaseWalletSDK, 
+   options: {
+     appName: "Web 3 Modal Demo",
+     infuraId: process.env.INFURA_KEY 
+   }
+ },
+ walletconnect: {
+   package: WalletConnect, 
+   options: {
+     infuraId: process.env.INFURA_KEY 
+   }
+ }
+};
+const web3Modal = new Web3Modal({
+  providerOptions // required
+});
+
 
 export default MyApp
